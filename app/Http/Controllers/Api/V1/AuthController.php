5d6f1d625ece15b0e5c8392753b993984656cbe4
柚子 <106157421@qq.com>
2021-01-11 16:46:15 +0800

    123

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -96,7 +96,13 @@ class AuthController extends Controller
 
         $user = User::where('ml_openid',$session['ml_openid'])->firstOrFail();
         $phoneNumber = $decryptedData['phoneNumber'];
-        $user->update(['phone'=>$phoneNumber]);
+        $user->update([
+            'phone'=>$phoneNumber,
+            'avatar'=>$request->avatar,
+            'nickname'=>$request->nickname,
+            'city'=>$request->city,
+            'sex'=>$request->sex,
+        ]);
 
         $token = \Auth::guard('api')->fromUser($user);
         return $this->respondWithToken($token,$phoneNumber,$user)->setStatusCode(201);
