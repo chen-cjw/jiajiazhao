7b8e691ef24ac2c0da033f59c0b5b01a2c4baa2f
柚子 <106157421@qq.com>
2021-01-15 18:03:07 +0800

    123

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,22 +11,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UserSeeder::class);
-         $this->call(CardCategorySeeder::class);
+//         $this->call(UserSeeder::class);
+//         $this->call(CardCategorySeeder::class);
          $this->call(AbbrCategorySeeder::class);
-         $this->call(LocalCarpoolingSeeder::class);
-         $this->call(NoticeSeeder::class);
-         $this->call(BannerSeeder::class);
-         $this->call(ConvenientInformationSeeder::class);
-         $this->call(ShopSeeder::class);
-         $this->call(SettingSeeder::class);
-         $this->call(BannerSeeder::class);
-         $this->call(AdvertisingSpaceSeeder::class);
-         $this->call(PostDescriptionSeeder::class);
-         $this->call(CommentSeed::class);
-        $this->call(CarpoolingSeed::class);
-        $this->call(AdminTablesSeeder::class);
-        $this->call(DialingSeed::class);
-        $this->call(AbbrTwoCategorySeeder::class);
+//         $this->call(LocalCarpoolingSeeder::class);
+//         $this->call(NoticeSeeder::class);
+//         $this->call(BannerSeeder::class);
+//         $this->call(ConvenientInformationSeeder::class);
+//         $this->call(ShopSeeder::class);
+//         $this->call(SettingSeeder::class);
+//         $this->call(BannerSeeder::class);
+//         $this->call(AdvertisingSpaceSeeder::class);
+//         $this->call(PostDescriptionSeeder::class);
+//         $this->call(CommentSeed::class);
+//        $this->call(CarpoolingSeed::class);
+//        $this->call(AdminTablesSeeder::class);
+//        $this->call(DialingSeed::class);
+//        $this->call(AbbrTwoCategorySeeder::class);
     }
 }
