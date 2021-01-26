ca9967b5a42d53a2b5918156523a8af047f004da
柚子 <106157421@qq.com>
2021-01-26 11:04:34 +0800

    123

--- app/Admin/Controllers/AbbrCategoryController.php
+++ app/Admin/Controllers/AbbrCategoryController.php
@@ -121,6 +121,27 @@ class AbbrCategoryController extends AdminController
 
         }
 
+
+        $form->footer(function ($footer) {
+
+            // 去掉`重置`按钮
+            $footer->disableReset();
+
+            // 去掉`提交`按钮
+            $footer->disableSubmit();
+
+            // 去掉`查看`checkbox
+            $footer->disableViewCheck();
+
+            // 去掉`继续编辑`checkbox
+            $footer->disableEditingCheck();
+
+            // 去掉`继续创建`checkbox
+            $footer->disableCreatingCheck();
+
+        });
+
+
         return $form;
     }
 }
