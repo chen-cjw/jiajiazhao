bb4e166b560c2ae23db83401a053650cc888921b
柚子 <106157421@qq.com>
2021-01-26 20:38:53 +0800

    123

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,29 +12,29 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(UserSeeder::class);
-//         $this->call(CardCategorySeeder::class);
-//         $this->call(AbbrCategorySeeder::class);
-//         $this->call(LocalCarpoolingSeeder::class);
-//         $this->call(NoticeSeeder::class);
-//         $this->call(BannerSeeder::class);
-//         $this->call(ConvenientInformationSeeder::class);
-//         $this->call(ShopSeeder::class);
+         $this->call(CardCategorySeeder::class);
+         $this->call(AbbrCategorySeeder::class);
+         $this->call(LocalCarpoolingSeeder::class);
+         $this->call(NoticeSeeder::class);
+         $this->call(BannerSeeder::class);
+         $this->call(ConvenientInformationSeeder::class);
+         $this->call(ShopSeeder::class);
          $this->call(SettingSeeder::class);
-//         $this->call(BannerSeeder::class);
-//         $this->call(AdvertisingSpaceSeeder::class);
-//         $this->call(PostDescriptionSeeder::class);
-//         $this->call(CommentSeed::class);
-//        $this->call(CarpoolingSeed::class);
+         $this->call(BannerSeeder::class);
+         $this->call(AdvertisingSpaceSeeder::class);
+         $this->call(PostDescriptionSeeder::class);
+         $this->call(CommentSeed::class);
+        $this->call(CarpoolingSeed::class);
         $this->call(AdminTablesSeeder::class);
-//        $this->call(DialingSeed::class);
+        $this->call(DialingSeed::class);
         $this->call(AbbrTwoCategorySeeder::class);
-//        $this->call(UserFavoriteCardSeeder::class);
-//        $this->call(UserFavoriteShopSeeder::class);
-//        $this->call(PostTipSeeder::class);
-//        $this->call(BannerInformationShowSeeder::class);
-//        $this->call(BannerPersonSeeder::class);
-//        $this->call(MerchantEnteringAgreementSeeder::class);
-//        $this->call(MerchantPrivacyAgreementSeeder::class);
-//        $this->call(BannerLocalSeeder::class);
+        $this->call(UserFavoriteCardSeeder::class);
+        $this->call(UserFavoriteShopSeeder::class);
+        $this->call(PostTipSeeder::class);
+        $this->call(BannerInformationShowSeeder::class);
+        $this->call(BannerPersonSeeder::class);
+        $this->call(MerchantEnteringAgreementSeeder::class);
+        $this->call(MerchantPrivacyAgreementSeeder::class);
+        $this->call(BannerLocalSeeder::class);
     }
 }
