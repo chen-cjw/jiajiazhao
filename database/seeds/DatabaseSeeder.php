57fae33328cd67a0f3a81711a7dca1b82f21fef5
柚子 <106157421@qq.com>
2021-01-26 10:36:11 +0800

    123

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,15 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         $this->call(UserSeeder::class);
+         $this->call(UserSeeder::class);
 //         $this->call(CardCategorySeeder::class);
-//         $this->call(AbbrCategorySeeder::class);
+         $this->call(AbbrCategorySeeder::class);
 //         $this->call(LocalCarpoolingSeeder::class);
 //         $this->call(NoticeSeeder::class);
 //         $this->call(BannerSeeder::class);
 //         $this->call(ConvenientInformationSeeder::class);
-         $this->call(ShopSeeder::class);
-//         $this->call(SettingSeeder::class);
+//         $this->call(ShopSeeder::class);
+         $this->call(SettingSeeder::class);
 //         $this->call(BannerSeeder::class);
 //         $this->call(AdvertisingSpaceSeeder::class);
 //         $this->call(PostDescriptionSeeder::class);
