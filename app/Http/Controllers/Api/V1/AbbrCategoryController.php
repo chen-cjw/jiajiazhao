85ef166ed700e4abef4ff91dbd192ded0df3cab3
柚子 <106157421@qq.com>
2021-01-27 17:42:04 +0800

    123

--- app/Http/Controllers/Api/V1/AbbrCategoryController.php
+++ app/Http/Controllers/Api/V1/AbbrCategoryController.php
@@ -10,7 +10,7 @@ class AbbrCategoryController extends Controller
     // 行业分类(后台)
     public function index()
     {
-        $abbrCategory = AbbrCategory::orderBy('sort','desc')->where('parent_id',null)->get();
+        $abbrCategory = AbbrCategory::orderBy('sort','desc')->where('type','shop')->where('parent_id',null)->get();
         return $this->responseStyle('ok',200,$abbrCategory);
         return $this->response->collection($abbrCategory,new AbbrCategoryTransformer());
     }
