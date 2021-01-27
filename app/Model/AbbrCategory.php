0607b9bdaa11f7e4d25e01405e61b55485833a14
柚子 <106157421@qq.com>
2021-01-27 13:53:46 +0800

    123

--- app/Model/AbbrCategory.php
+++ app/Model/AbbrCategory.php
@@ -9,6 +9,7 @@ class AbbrCategory extends Model
     // 行业分类(后台)
     protected $fillable = ['abbr','sort','logo','parent_id','type','local'];
 
+
     public function abbrs()
     {
         return $this->hasMany(AbbrCategory::class,'parent_id','id');
@@ -28,10 +29,12 @@ class AbbrCategory extends Model
         return \Storage::disk('public')->url($image);
     }
 
-    public function setImageAttribute($pictures)
+    public function setImageAttribute($images)
     {
-        if (is_array($pictures)) {
-            $this->attributes['image'] = json_encode($pictures);
+//        dd($images);
+//        return $images;
+        if (is_array($images)) {
+            $this->attributes['image'] = json_encode($images);
         }
     }
 
@@ -44,8 +47,9 @@ class AbbrCategory extends Model
         foreach ($data as $k=>$v) {
             if (Str::startsWith($v, ['http://', 'https://'])) {
                 $da[] = $v;
+            }else {
+                $da[] = \Storage::disk('public')->url($v);
             }
-            $da[] = \Storage::disk('public')->url($v);
         }
         return $da;
     }
