106915339d6debddf6d13e476d4473c4b490bdbd
柚子 <106157421@qq.com>
2021-01-28 17:47:43 +0800

    123

--- app/Model/History.php
+++ app/Model/History.php
@@ -10,19 +10,22 @@ class History extends Model
     {
         if($this->attributes['model_type'] == Shop::class) {
             $res = Shop::where('id',$this->attributes['id'])->first();
-            $lat1 = $res->lat;
-            $lng1 = $res->lng;
-            $lat = \request('lat');
-            $lng = \request('lng');
-            if ($lng && $lat) {
-                $range = $this->getDistance($lat,$lng,$lat1,$lng1);
-            }else {
-                $range = '未知';
+            if ($res) {
+                $lat1 = $res->lat;
+                $lng1 = $res->lng;
+                $lat = \request('lat');
+                $lng = \request('lng');
+                if ($lng && $lat) {
+                    $range = $this->getDistance($lat,$lng,$lat1,$lng1);
+                }else {
+                    $range = '未知';
+                }
+                // 几公里
+                $res->range = $range;
+                // 平均星级
+                $res->favoriteShopStarSvg=number_format(ShopComment::where('shop_id',$res->id)->avg('star'),1) ;
             }
-            // 几公里
-            $res->range = $range;
-            // 平均星级
-            $res->favoriteShopStarSvg=number_format(ShopComment::where('shop_id',$res->id)->avg('star'),1) ;
+
         }
         if($this->attributes['model_type'] == LocalCarpooling::class) {
             $res = LocalCarpooling::where('id',$this->attributes['id'])->first();
