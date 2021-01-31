0e12b0eb018d0c7bde5334334ddad7cdc9e60969
柚子 <106157421@qq.com>
2021-01-31 15:20:31 +0800

    123

--- app/Model/Dialing.php
+++ app/Model/Dialing.php
@@ -11,32 +11,37 @@ class Dialing extends Model
         return $this->morphTo(Shop::class,'model_type','model_id');
     }
 
-    public function getModelAttribute()
+    public function getModelTypeAttribute()
     {
-        if ($this->attributes['model_type']==Shop::class) {
+        if($this->attributes['model_type'] == Shop::class) {
             $res = Shop::where('id',$this->attributes['model_id'])->first();
             if ($res) {
-
-                $lat = request('lat');
-                $lng = request('lng');
-                // 几公里
-                if ($lat && $lng) {
-
-                    $res['range'] = $this->getDistance($lat, $lng, $res['lat'], $res['lng']);
-                } else {
-                    $res['range'] = '未知';
+                $lat1 = $res->lat;
+                $lng1 = $res->lng;
+                $lat = \request('lat');
+                $lng = \request('lng');
+                if ($lng && $lat) {
+                    $range = $this->getDistance($lat,$lng,$lat1,$lng1);
+                }else {
+                    $range = '未知';
                 }
+                // 几公里
+                $res->range = $range;
                 // 平均星级
-                $res['favoriteShopStarSvg'] = number_format(ShopComment::where('shop_id', $this->attributes['model_id'])->avg('star'), 1);
-                return $res;
+                $res->favoriteShopStarSvg=number_format(ShopComment::where('shop_id',$res->id)->avg('star'),1) ;
             }
-        }
-        if ($this->attributes['model_type']==LocalCarpooling::class) {
-            return LocalCarpooling::where('id',$this->attributes['model_id'])->first();
-        }
 
+        }
+        if($this->attributes['model_type'] == LocalCarpooling::class) {
+            $res = LocalCarpooling::where('id',$this->attributes['model_id'])->first();
+        }
+        if($this->attributes['model_type'] == ConvenientInformation::class) {
+            $res = ConvenientInformation::where('id',$this->attributes['model_id'])->first();
+        }
+        return $res;
     }
-    protected $appends = ['model'];
+
+
     /**
      * @desc 根据两点间的经纬度计算距离
      * @param float $lat 纬度值
