ebeb90e0946234df2d193cf749491d1de4848882
柚子 <106157421@qq.com>
2021-02-04 15:14:53 +0800

    123

--- app/Http/Controllers/Api/V1/MakeQrCodeController.php
+++ app/Http/Controllers/Api/V1/MakeQrCodeController.php
@@ -10,62 +10,12 @@ use Illuminate\Support\Str;
 
 class MakeQrCodeController extends Controller
 {
-    public function accessToken()
-    {
-        $appid = "wx693aa465df66510b";
-        $appsecret = "058b6ee18b85ecd12a93c49ccd7fac28";
-        $isExpires = $this->isExpires();
-        if($isExpires === false){
-            //到期，获取新的
-            $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' . $appid . '&secret=' . $appsecret;
-            $res = $this->curl($url);
-            // dump($res);
-            $arr = json_decode($res,true);
-            if($arr && !isset($arr['errcode'])){
-                $arr['time'] = time();
-                file_put_contents(public_path() . '/access_token.json', json_encode($arr));
-                return $arr['access_token'];
-            }else{
-                echo 'error on get access_token';die;
-            }
-        }else{
-            return $isExpires;
-        }
-
-
-    }
-
-    public function makeBack(Request $request)
-    {
-        //图片一
-        $path_1 = 'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png';
-//图片二
-        $path_2 = 'http://tb1.bdstatic.com/tb/static-client/img/webpage/wap_code.png';
-        $image_3 = '';
-//创建图片对象
-//imagecreatefrompng($filename)--由文件或 URL 创建一个新图象
-        $image_1 = imagecreatefrompng($path_1);
-        $image_2 = imagecreatefrompng($path_2);
-//合成图片
-//imagecopymerge ( resource $dst_im , resource $src_im , int $dst_x , int $dst_y , int $src_x , int $src_y , int $src_w , int $src_h , int $pct )---拷贝并合并图像的一部分
-//将 src_im 图像中坐标从 src_x，src_y 开始，宽度为 src_w，高度为 src_h 的一部分拷贝到 dst_im 图像中坐标为 dst_x 和 dst_y 的位置上。两图像将根据 pct 来决定合并程度，其值范围从 0 到 100。当 pct = 0 时，实际上什么也没做，当为 100 时对于调色板图像本函数和 imagecopy() 完全一样，它对真彩色图像实现了 alpha 透明。
-        imagecopymerge($image_1, $image_2, 0, 0, 0, 0, imagesx($image_2), imagesy($image_2), 100);
-// 输出合成图片
-//imagepng($image[,$filename]) — 以 PNG 格式将图像输出到浏览器或文件
-        $merge = 'merge.png';
-
-        var_dump(imagepng("http://admin.jiajiazhao.dev/storage/20210202173941-60191ddd41525.png","http://admin.jiajiazhao.dev/storage/20210202173941-60191ddd41525.png"));
-
-    }
-
-
 
     public function makeShare(Request $request)
     {
-
         $config = [
             'app_id' => 'wx693aa465df66510b',
-            'secret' => '058b6ee18b85ecd12a93c49ccd7fac28',
+            'secret' => config('wechat.mini_program.default.secret'),
 
             // 下面为可选项
             // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
