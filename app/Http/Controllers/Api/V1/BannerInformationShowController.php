9b5fe971b08602c8c8cff094269a8e990df13a59
柚子 <106157421@qq.com>
2021-02-08 14:48:00 +0800

    123

--- app/Http/Controllers/Api/V1/BannerInformationShowController.php
+++ app/Http/Controllers/Api/V1/BannerInformationShowController.php
@@ -15,9 +15,10 @@ class BannerInformationShowController extends Controller
         Log::info(request('card_id'));
         // 同类推荐
         $con = ConvenientInformation::where('id',request('card_id'))->first();
-        Log::info($con);
-        Log::info($con->card_id);
-        $bannerInformationShowOne = ConvenientInformation::where('is_display',1)->where('card_id',$con->card_id)->orderBy('sort','desc')->whereNotNull('paid_at')->take(5)->get();//->where('title','like','%'.request('title').'%')
+//        Log::info($con);
+//        Log::info($con->card_id);
+        // 这里的逻辑不能随意改
+        $bannerInformationShowOne = ConvenientInformation::where('is_display',1)->where('card_id',$con->card_id->id)->orderBy('sort','desc')->whereNotNull('paid_at')->take(5)->get();//->where('title','like','%'.request('title').'%')
 
         //        $bannerInformationShowOne = BannerInformationShow::where('is_display',1)->where('type','one')->orderBy('sort','desc')->get();
         $bannerInformationShowTwo = BannerInformationShow::where('is_display',1)->where('type','two')->orderBy('sort','desc')->get();
