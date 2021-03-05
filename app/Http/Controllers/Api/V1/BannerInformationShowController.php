a712a7c190cde6881d28effc6744838717f99f6f
柚子 <106157421@qq.com>
2021-03-05 09:30:45 +0800

    推荐位

--- app/Http/Controllers/Api/V1/BannerInformationShowController.php
+++ app/Http/Controllers/Api/V1/BannerInformationShowController.php
@@ -19,9 +19,17 @@ class BannerInformationShowController extends Controller
         Log::info($con->card_id);
         Log::info($con->card_id->id);
         // 这里的逻辑不能随意改
-        if ($con) {
-            $bannerInformationShowOne = ConvenientInformation::where('is_display',1)->where('card_id',$con->card_id->id)->orderBy('sort','desc')->whereNotNull('paid_at')->take(5)->get();//->where('title','like','%'.request('title').'%')
+        $area = \request()->area;
 
+        if ($con) {
+             $query = ConvenientInformation::where('is_display',1)->where('card_id',$con->card_id->id)->orderBy('sort','desc')->whereNotNull('paid_at')->take(5);//->where('title','like','%'.request('title').'%')
+
+            if (config('app.city') == 1) {
+                if ($area != '') {
+                    $query = $query->where('location', 'like', '%' . $area . '%');
+                }
+            }
+            $bannerInformationShowOne = $query->get();
         }else {
             $bannerInformationShowOne = ConvenientInformation::where('is_display',2)->get();
         }
