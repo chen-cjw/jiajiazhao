7ae3b513ba46b369af2678949e1d97885a10283c
柚子 <106157421@qq.com>
2021-06-28 13:56:01 +0800

    提交入住订单

--- app/Http/Controllers/Api/V1/CityPartnerController.php
+++ app/Http/Controllers/Api/V1/CityPartnerController.php
@@ -86,10 +86,11 @@ class CityPartnerController extends Controller
         $data['amount'] = 1;// todo 获取默认配置
         if ($id = CityPartner::where('user_id',$data['user_id'])->whereNull('paid_at')->value('id')) {
             $res =  CityPartner::where('id',$id)->update($data);
+            return ['code'=>200,'msg'=>'ok','data'=>CityPartner::where('id',$id)->first()];
         }else {
             $res = CityPartner::create($data);
+            return ['code'=>200,'msg'=>'ok','data'=>$res];
         }
-        return ['code'=>200,'msg'=>'ok','data'=>$res];
 
     }
 
