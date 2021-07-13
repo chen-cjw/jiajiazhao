0da08039caeca78fc64388dfda444f63963aacf9
柚子 <106157421@qq.com>
2021-07-13 16:37:10 +0800

    生成假的测试数据

--- app/Model/CityPayOrder.php
+++ app/Model/CityPayOrder.php
@@ -2,9 +2,12 @@
 
 namespace App\Model;
 
-use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class CityPayOrder extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
