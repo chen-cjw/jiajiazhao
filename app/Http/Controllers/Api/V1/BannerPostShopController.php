58d287f821194ab3d7520dbbad544a23ee62609e
柚子 <106157421@qq.com>
2021-07-23 08:43:48 +0800

    area

--- app/Http/Controllers/Api/V1/BannerPostShopController.php
+++ app/Http/Controllers/Api/V1/BannerPostShopController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Model\BannerPostShop;
+use Illuminate\Support\Facades\Log;
 
 class BannerPostShopController extends Controller
 {
     public function index()
     {
+        Log::info(123);
+        Log::info(request('area'));
+        Log::info(123);
         $resQuery = BannerPostShop::where('is_display',1)->orderBy('sort','desc');
         if (request('area')) {
             $resQuery = $resQuery->where(function ($query) {
