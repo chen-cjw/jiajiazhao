5375c1b23ef89d9196930845c217de25e2487d07
柚子 <106157421@qq.com>
2021-07-28 14:40:56 +0800

    提现假数据

--- app/Model/AB/ChinaArea.php
+++ app/Model/AB/ChinaArea.php
@@ -9,8 +9,15 @@ class ChinaArea extends Model
     protected $table = 'china_areas';
     public function getNicknameAttribute()
     {
+        $rand = rand(1,5);
+        if ($rand==1) {
+            return 'xxxx'.'市合伙人';
+        }else if ($rand==2) {
+            return 'xxxx'.'县合伙人';
+        }else{
+            return 'xxxx'.'区合伙人';
+        }
 //        return $this->attributes['name'].'合伙人';
-        return 'xxxx'.'合伙人';
     }
     protected $appends = ['nickname'];
 }
