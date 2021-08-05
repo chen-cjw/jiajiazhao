2b76c72cd8c20506bb01f0c1115f8a3682b2a3ed
柚子 <106157421@qq.com>
2021-08-05 14:52:30 +0800

    城市合伙人轮播

--- app/Model/PartnerBanner.php
+++ app/Model/PartnerBanner.php
@@ -6,4 +6,10 @@ namespace App\Model;
 class PartnerBanner extends Model
 {
     protected $fillable = ['area'];
+
+    public function getLinkAttribute()
+    {
+        return $this->attributes['link_url'];
+    }
+    protected $appends = ['link'];
 }
